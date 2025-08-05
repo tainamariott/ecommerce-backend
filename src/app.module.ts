3587dbd29e9e42d20488a59fd231b6274a77a5b6
Tainá Cristiane Mariott <mariotttaina@gmail.com>
2025-08-04 21:50:45 -0300

    -feat: Conexão com o banco de dados (aula 04/08)

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host:'aws-0-sa-east-1.pooler.supabase.com',
+      port: +'5432',
+      username: 'postgres.tvwaeokfmvdxlinlyhgv',
+      password: 'TainaMariott1',
+    }),
+  ],
+})
+export class AppModule {}
